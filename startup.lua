48f5e7a50193e503b0a7d934043419f16537b379
hanamaru5626-art <hanamaru5626@gmail.com>
2026-02-06 21:51:06 +0900

    Add files via upload

---
+++ startup.lua
@@ -0,0 +1,25 @@
+local url = "https://github.com/hanamaru5626-art/CCTweaked_lua-program/"
+local response = http.get(url)
+if not response then
+    term.clear()
+    term.setCursorPos(1,1)
+    print("Sorry, something went wrong. ")
+    print("Please check your internet connection or restart. ")
+    print("If the problem persists, report the problem to a technician.")
+    print("00")
+else
+    local code = response.readAll()
+    response.close()
+    
+    local fn, err = load(code, url)
+    if not fn then
+        term.clear()
+        term.setCursorPos(1,1)
+        print("Sorry, something went wrong. ")
+        print("Please check your internet connection or restart. ")
+        print("If the problem persists, report the problem to a technician.")
+        print("01")
+    else
+        fn()
+    end
+end
