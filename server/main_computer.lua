e51b1affab07404b05ab8b63de130e47e68bebfa
hanamaru5626-art <hanamaru5626@gmail.com>
2026-02-10 21:32:56 +0900

    Update main_computer.lua

--- server/main_computer.lua
+++ server/main_computer.lua
@@ -1,6 +1,7 @@
 term.clear()
 term.setCursorPos(1,1)
-local modem =peripheral.find("modem")
+
+local modem = peripheral.find("modem")
 local MAIN_CHANNEL = 0
 local DNS_CHANNEL  = 1
 
@@ -9,15 +10,30 @@ modem.open(MAIN_CHANNEL)
 print("starting main computer")
 
 while true do
-    local _, _, ch, reply, msg =os.pullEvent("modem_message")
-    print("recv ch:", ch, "reply:", reply)
-    print(textutils.serialize(msg))
-    if ch ~= MAIN_CHANNEL or type(msg) ~="table" then goto continue end
-    local dest = msg.to
-    if tonumber(dest) then
-        modem.transmit(tonumber(dest),msg.from,msg)
-    else
-        modem.transmit(DNS_CHANNEL,MAIN_CHANNEL,msg)
+    local _, side, ch, reply, msg = os.pullEvent("modem_message")
+
+    if ch ~= MAIN_CHANNEL or type(msg) ~= "table" then
+        goto continue
     end
+
+    local dest = msg.to
+
+    -- 受信ログ
+    print(("[RECV] from:%s to:%s ch:%d"):format(
+        tostring(msg.from),
+        tostring(msg.to),
+        ch
+    ))
+
+    if tonumber(dest) then
+        modem.transmit(tonumber(dest), msg.from, msg)
+        print("[SEND] direct ->", dest)
+    else
+        modem.transmit(DNS_CHANNEL, MAIN_CHANNEL, msg)
+        print("[SEND] DNS ->", DNS_CHANNEL)
+    end
+
+    print("----------------------")
+
     ::continue::
 end
